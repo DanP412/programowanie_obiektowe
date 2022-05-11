a4ad015f5b4450fda17cdf097e5351ec847b68dc
Daniel Partatus <Daniel.Partatus@microsoft.wsei.edu.pl>
2022-05-11 21:06:56 +0200

    Latest commit 310e841 on 11 Mar

--- lab_01/lab_01/Program.cs
+++ lab_01/lab_01/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 
-namespace lab_01
+namespace prog_obj_01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Test");
+
         }
+
     }
+
+
+    class Ulamek
+    {
+        Ulamek ulamek1 = new Ulamek(5, 3);
+
+        private int mianownik { get; set; }
+        private int licznik { get; set; }
+
+
+
+        public Ulamek()
+        {
+
+        }
+
+
+        public Ulamek(int test, int test2)
+        {
+            test = 2;
+            test2 = 4;
+        }
+
+        public Ulamek(Ulamek kopiaUlamka)
+        {
+            ulamek1 = kopiaUlamka.ulamek1;
+        }
+
+
+        public static Ulamek operator +(Ulamek licz, Ulamek mian)
+        {
+            return new Ulamek(licz.)
+        }
+
+
+    }
+
+
+
 }
